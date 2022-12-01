7881021a4b67d722da39fc59a02c24ae31c42991
Vincent Alsteen <vincent.alsteen@deliveryhero.com>
2022-12-01 09:36:55 +0100

    memory optimization

--- day1/part1/src/main.rs
+++ day1/part1/src/main.rs
@@ -1,16 +1,23 @@
 use std::error::Error;
 use std::io::{stdin, BufRead};
+use std::iter::once;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let lines = stdin().lock().lines();
-    let calories = lines.flatten().try_fold(vec![0], |mut acc, line| {
-        if line.is_empty() {
-            acc.push(0)
-        } else {
-            *acc.last_mut().ok_or("expected integer")? += line.parse::<usize>()?
-        }
-        Ok::<_, Box<dyn Error>>(acc)
-    })?;
-    println!("{}", calories.iter().max().ok_or("cannot find maximum")?);
+    let (max, _) = lines
+        .flatten()
+        .chain(once(String::new()))
+        .try_fold((0, 0), |(max, current), line| {
+            if line.is_empty() {
+                if current > max {
+                    Ok((current, 0))
+                } else {
+                    Ok((max, 0))
+                }
+            } else {
+                Ok::<_, Box<dyn Error>>((max, current + line.parse::<usize>()?))
+            }
+        })?;
+    println!("{}", max);
     Ok(())
 }
--- day1/part2/src/main.rs
+++ day1/part2/src/main.rs
@@ -1,24 +1,24 @@
-use std::cmp::Reverse;
 use std::error::Error;
 use std::io::{stdin, BufRead};
+use std::iter::once;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let lines = stdin().lock().lines();
-    let mut calories = lines.flatten().try_fold(vec![0], |mut acc, line| {
-        if line.is_empty() {
-            acc.push(0)
-        } else {
-            *acc.last_mut().ok_or("expected integer")? += line.parse::<usize>()?
-        }
-        Ok::<_, Box<dyn Error>>(acc)
-    })?;
-    calories.sort_by_key(|c| Reverse(*c));
-    let sum_top_three: usize = calories
-        .chunks_exact(3)
-        .next()
-        .ok_or("not enough elements")?
-        .iter()
-        .sum();
-    println!("{}", sum_top_three);
+    let (sum_top_three, _) = lines.flatten().chain(once(String::new())).try_fold(
+        (vec![], 0),
+        |(mut result, current), line| {
+            if line.is_empty() {
+                let pos = result
+                    .binary_search_by(|e| current.cmp(e))
+                    .unwrap_or_else(|e| e);
+                result.insert(pos, current);
+                result.truncate(3);
+                Ok((result, 0))
+            } else {
+                Ok::<_, Box<dyn Error>>((result, current + line.parse::<usize>()?))
+            }
+        },
+    )?;
+    println!("{}", sum_top_three.iter().sum::<usize>());
     Ok(())
 }
