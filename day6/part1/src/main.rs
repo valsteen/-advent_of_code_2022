58d2c95b1d28118fbfc20686e5d2717519d88412
Vincent Alsteen <vincent.alsteen@deliveryhero.com>
2022-12-06 10:17:03 +0100

    isolate len

--- day6/part1/src/main.rs
+++ day6/part1/src/main.rs
@@ -1,16 +1,21 @@
 use std::collections::hash_map::RandomState;
 use std::collections::HashSet;
 use std::error::Error;
-use std::io::{BufRead, stdin};
+use std::io::{stdin, BufRead};
+
+const LEN: usize = 4;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let lines = stdin().lock().lines();
-    let positions = lines.flatten().filter_map( |line| {
-        (3..line.len()).find(|pos| {
-            let chars = HashSet::<_, RandomState>::from_iter(line[pos-3..=*pos].chars());
-            chars.len() == 4
+    let positions = lines
+        .flatten()
+        .filter_map(|line| {
+            (LEN - 1..line.len()).find(|pos| {
+                HashSet::<_, RandomState>::from_iter(line[pos + 1 - LEN..=*pos].chars()).len()
+                    == LEN
+            })
         })
-    } ).map(|c| c+1);
+        .map(|c| c + 1);
 
     for pos in positions {
         println!("{}", pos)
--- day6/part2/src/main.rs
+++ day6/part2/src/main.rs
@@ -1,16 +1,21 @@
 use std::collections::hash_map::RandomState;
 use std::collections::HashSet;
 use std::error::Error;
-use std::io::{BufRead, stdin};
+use std::io::{stdin, BufRead};
+
+const LEN: usize = 14;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let lines = stdin().lock().lines();
-    let positions = lines.flatten().filter_map( |line| {
-        (13..line.len()).find(|pos| {
-            let chars = HashSet::<_, RandomState>::from_iter(line[pos-13..=*pos].chars());
-            chars.len() == 14
+    let positions = lines
+        .flatten()
+        .filter_map(|line| {
+            (LEN - 1..line.len()).find(|pos| {
+                HashSet::<_, RandomState>::from_iter(line[pos + 1 - LEN..=*pos].chars()).len()
+                    == LEN
+            })
         })
-    } ).map(|c| c+1);
+        .map(|c| c + 1);
 
     for pos in positions {
         println!("{}", pos)
