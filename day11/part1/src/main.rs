3c6348b27eb1246ed856637ee5589ff64f5380ed
Vincent Alsteen <vincent.alsteen@deliveryhero.com>
2022-12-11 14:37:43 +0100

    day 11 part1

--- day11/part1/src/main.rs
+++ day11/part1/src/main.rs
@@ -1,6 +1,8 @@
+use num_integer::Integer;
 use recap::Recap;
 use serde::Deserialize;
 use serde_with::{serde_as, DisplayFromStr};
+use std::cmp::Reverse;
 use std::error::Error;
 use std::io::{stdin, BufRead};
 use std::str::FromStr;
@@ -11,10 +13,29 @@ struct MonkeyNumber {
     n: usize,
 }
 
-#[derive(Debug, Deserialize, Recap)]
-#[recap(regex = r#"^  Starting items: ((?P<level>[0-9]+)(, )?)+$"#)]
+#[derive(Debug)]
 struct Items {
     level: Vec<usize>,
+}
+
+impl FromStr for Items {
+    type Err = Box<dyn Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let re = regex::Regex::new(r#"^  Starting items: (?P<levels>(\d+(, )?)+)$"#)?;
+        let items = re
+            .captures(s)
+            .ok_or("no match")?
+            .name("levels")
+            .ok_or("no items")?;
+        Ok(Self {
+            level: items
+                .as_str()
+                .split(", ")
+                .map(|s| Ok(s.parse()?))
+                .collect::<Result<_, Box<dyn Error>>>()?,
+        })
+    }
 }
 
 #[derive(Debug)]
@@ -91,46 +112,93 @@ struct Monkey {
     divisible: usize,
     if_true: usize,
     if_false: usize,
+    inspections: usize,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let lines = stdin().lock().lines();
     let lines = lines.flatten().filter(|line| !line.is_empty());
-    let monkeys = (0..).scan(lines, |lines, n| {
-        match MonkeyNumber::from_str(&lines.next()?) {
-            Ok(monkey) => {
-                if monkey.n != n {
-                    return Some(Err("unexpected monkey number".into()));
+    let mut monkeys = (0..)
+        .scan(lines, |lines, n| {
+            match MonkeyNumber::from_str(&lines.next()?) {
+                Ok(monkey) => {
+                    if monkey.n != n {
+                        return Some(Err("unexpected monkey number".into()));
+                    }
                 }
+                Err(e) => return Some(Err(e.into())),
             }
-            Err(e) => return Some(Err(e.into())),
+            let mut parse_monkey = || {
+                let items = Items::from_str(&lines.next().ok_or("expected items")?)?.level;
+                let operation = Operation::from_str(&lines.next().ok_or("expected operation")?)?;
+                let divisible = Test::from_str(&lines.next().ok_or("expected test")?)?.divisible;
+                let condition_true = Condition::from_str(&lines.next().ok_or("expected test")?)?;
+                if !condition_true.boolean {
+                    Err("expected true condition")?
+                }
+                let condition_false = Condition::from_str(&lines.next().ok_or("expected test")?)?;
+                if condition_false.boolean {
+                    Err("expected false condition")?
+                }
+                Ok(Monkey {
+                    items,
+                    operation,
+                    divisible,
+                    if_true: condition_true.monkey,
+                    if_false: condition_false.monkey,
+                    inspections: 0,
+                })
+            };
+
+            Some(parse_monkey())
+        })
+        .collect::<Result<Vec<Monkey>, Box<dyn Error>>>()?;
+
+    for _ in 0..20 {
+        for monkey_number in 0..monkeys.len() {
+            let operations = {
+                let monkey = monkeys.get_mut(monkey_number).ok_or("missing monkey")?;
+                let operations = monkey
+                    .items
+                    .drain(..)
+                    .map(|item| {
+                        let operand = match monkey.operation.operand {
+                            Operand::Number(n) => n,
+                            Operand::Old => item,
+                        };
+                        let level = match monkey.operation.operator {
+                            Operator::Plus => item + operand,
+                            Operator::Multiply => item * operand,
+                        } / 3;
+                        if level.is_multiple_of(&monkey.divisible) {
+                            (level, monkey.if_true)
+                        } else {
+                            (level, monkey.if_false)
+                        }
+                    })
+                    .collect::<Vec<_>>();
+                monkey.inspections += operations.len();
+                operations
+            };
+            for (level, destination) in operations {
+                monkeys
+                    .get_mut(destination)
+                    .ok_or("missing destination monkey")?
+                    .items
+                    .push(level);
+            }
         }
-        let mut parse_monkey = || {
-            let items = Items::from_str(&lines.next().ok_or("expected items")?)?.level;
-            let operation = Operation::from_str(&lines.next().ok_or("expected operation")?)?;
-            let divisible = Test::from_str(&lines.next().ok_or("expected test")?)?.divisible;
-            let condition_true = Condition::from_str(&lines.next().ok_or("expected test")?)?;
-            if !condition_true.boolean {
-                Err("expected true condition")?
-            }
-            let condition_false = Condition::from_str(&lines.next().ok_or("expected test")?)?;
-            if condition_false.boolean {
-                Err("expected false condition")?
-            }
-            Ok(Monkey {
-                items,
-                operation,
-                divisible,
-                if_true: condition_true.monkey,
-                if_false: condition_false.monkey,
-            })
-        };
-
-        Some(parse_monkey())
-    }).collect::<Result<Vec<Monkey>, Box<dyn Error>>>()?;
-
-    for monkey in monkeys {
-        println!("{:?}", monkey);
     }
+
+    let mut inspections = monkeys
+        .iter()
+        .map(|monkey| monkey.inspections)
+        .collect::<Vec<usize>>();
+    inspections.sort_by_key(|v| Reverse(*v));
+    inspections.truncate(2);
+    if inspections.len() != 2 {
+        Err("less than two monkeys")?
+    }
+    println!("{}", inspections[0] * inspections[1]);
     Ok(())
 }
