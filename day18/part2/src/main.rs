578966030b4ab1fa060fdb1308d436fa3ef10c33
Vincent Alsteen <vincent.alsteen@deliveryhero.com>
2022-12-18 16:39:46 +0100

    day 18 part 2

--- day18/part2/src/main.rs
+++ day18/part2/src/main.rs
@@ -1,28 +1,8 @@
+use std::cmp::Reverse;
 use std::collections::hash_map::RandomState;
 use std::collections::HashSet;
 use std::error::Error;
 use std::io::{stdin, BufRead};
-use std::iter::once_with;
-
-/*
-two approaches:
-- scan line by line. for all cubes, all non-cube neighbours are marked as air
-- starting from next line, every empty space touching air becomes air .. but then we need several
-  passes until the air are all marked
-
-- make a visitor. start from a free location next to a cube
-- iterate the 8 neighbours
-  - if at least one side is touching :
-    - mark all neighbours : if cube, add to surface counter, if air, ignore, if unmarked, mark as air and put in a queue
- */
-
-// it's not 2028 ... ?
-// not 2030 --
-/*
-That's not the right answer; your answer is too high.
-If you're stuck, make sure you're using the full input data; there are also some general tips on the about page,
-or you can ask for hints on the subreddit. Please wait one minute before trying again. (You guessed 2030.) [Return to Day 18]
- */
 
 fn edge_neighbours(cube: [i8; 3]) -> [[i8; 3]; 14] {
     [
@@ -54,12 +34,11 @@ fn neighbours(cube: [i8; 3]) -> [[i8; 3]; 6] {
     ]
 }
 
-fn exposed(cubes: Vec<[i8; 3]>) -> usize {
+fn exposed(cubes: Vec<[i8; 3]>, trapped: &mut HashSet<[i8; 3]>) -> usize {
     let Some(mut first) = cubes.iter().max().copied() else {
         return 0
     };
     first[0] += 1;
-    println!("start : {:?}", first);
     let cubes = HashSet::<_, RandomState>::from_iter(cubes);
     let mut air = HashSet::new();
 
@@ -77,47 +56,30 @@ fn exposed(cubes: Vec<[i8; 3]>) -> usize {
         air.insert(location);
 
         let neighbours = neighbours(location);
-        let touches = edge_neighbours(location).iter().any(|cube|  cubes.contains(cube));
+        let touches = edge_neighbours(location)
+            .iter()
+            .any(|cube| cubes.contains(cube));
         if !touches && !should_dive {
             continue;
         }
-        println!("{:?} {:?}", location, neighbours);
-        //assert_ne!([2,2,5], location);
         queue.extend(neighbours.into_iter().map(|cube| (cube, touches)));
     }
+
+    let mut trapped_queue = cubes
+        .iter()
+        .flat_map(|cube| neighbours(*cube))
+        .collect::<Vec<_>>();
+
+    while let Some(location) = trapped_queue.pop() {
+        if cubes.contains(&location) || trapped.contains(&location) || air.contains(&location) {
+            continue;
+        }
+        trapped.insert(location);
+        trapped_queue.extend(neighbours(location))
+    }
+
     exposed
 }
-//
-// fn rotations<T>(cubes: &'_ T) -> impl Iterator<Item = T> + '_
-// where
-//     T: 'static + AsMut<[[i8; 3]]> + Clone,
-// {
-//     once_with(|| cubes.clone())
-//         .chain(once_with(|| {
-//             let mut cubes = cubes.clone();
-//             cubes
-//                 .as_mut()
-//                 .iter_mut()
-//                 .for_each(|c| *c = [c[2], c[0], c[1]]);
-//             cubes
-//         }))
-//         .chain(once_with(|| {
-//             let mut cubes = cubes.clone();
-//             cubes
-//                 .as_mut()
-//                 .iter_mut()
-//                 .for_each(|c| *c = [c[1], c[2], c[0]]);
-//             cubes
-//         }))
-// }
-//
-// fn touches(left: [i8; 3], right: [i8; 3]) -> bool {
-//     rotations(&[left, right]).any(|cubes| {
-//         cubes[0][0] == cubes[1][0]
-//             && cubes[0][1] == cubes[1][1]
-//             && cubes[0][2].abs_diff(cubes[1][2]) == 1
-//     })
-// }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let cubes: Vec<[i8; 3]> = {
@@ -139,7 +101,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             .collect::<Result<_, Box<dyn Error>>>()?
     };
 
-    let groups = cubes
+    let mut groups = cubes
         .into_iter()
         .fold(vec![], |mut acc: Vec<Vec<[i8; 3]>>, cube| {
             let edge_neighbours = edge_neighbours(cube);
@@ -165,11 +127,19 @@ fn main() -> Result<(), Box<dyn Error>> {
             acc
         });
 
-    for group in &groups {
-        println!("{:?}", group);
-    }
+    groups.sort_by_key(|g| Reverse(g.len()));
+    let mut trapped = HashSet::new();
 
-    let result = groups.into_iter().map(exposed).sum::<usize>();
+    let result = groups
+        .into_iter()
+        .map(|cubes| {
+            if cubes.iter().any(|cube| trapped.contains(cube)) {
+                0
+            } else {
+                exposed(cubes, &mut trapped)
+            }
+        })
+        .sum::<usize>();
     println!("{}", result);
 
     Ok(())
